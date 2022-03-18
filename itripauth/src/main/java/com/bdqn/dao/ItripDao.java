3157757d4048db80d5044b1cf6eac0a14f152157
2045202219 <204520221@qq.com>
2022-03-18 15:00:49 +0800

    测试

---
+++ itripauth/src/main/java/com/bdqn/dao/ItripDao.java
@@ -0,0 +1,11 @@
+package com.bdqn.dao;
+
+import com.bdqn.pojo.ItripHotel;
+
+import java.util.List;
+
+public interface ItripDao {
+
+    public List<ItripHotel> list();
+    public List<ItripHotel> list1();
+}
